bf6f147258483dffa72cc6bef5af68ebd01b89a1
tscelsi <tscelsi@student.unimelb.edu.au>
2022-03-12 16:06:47 +1100

    created simple models to mimic user object and added fixtures with dummy data

--- app/models.py
+++ app/models.py
@@ -1,3 +1,28 @@
 from django.db import models
 
-# Create your models here.
+class Address(models.Model):
+    number = models.IntegerField()
+    # We'll use a TextField for all our text-based attributes. This doesn't enforce any
+    # max length constraints in the DB.
+    street = models.TextField()
+    city = models.TextField()
+    # let's make it so that we're only dealing with Australian states
+    state = models.CharField(
+        max_length=3,
+        choices=[
+            ("VIC", "Victoria"), 
+            ("NSW", "New South Wales"), 
+            ("QLD", "Queensland"), 
+            ("TAS", "Tasmania"), 
+            ("WA", "Western Australia"), 
+            ("NT", "Northern Territory"), 
+            ("SA", "South Australia")
+        ])
+
+
+class Person(models.Model):
+    email = models.EmailField(primary_key=True)
+    name = models.TextField()
+    # here we link an address to a person. If we remove the address, then we set the address of the 
+    # person to null.
+    address = models.ForeignKey(Address, related_name="person", on_delete=models.SET_NULL, null=True)
