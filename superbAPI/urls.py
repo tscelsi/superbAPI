9ecac78bf3a2d5776d6030ba15d8e61ff382e267
tscelsi <tscelsi@student.unimelb.edu.au>
2022-03-12 16:07:17 +1100

    mistakenly removed admin route

--- superbAPI/urls.py
+++ superbAPI/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
+from django.urls import path
+from django.contrib import admin
 from graphene_django.views import GraphQLView
 
 urlpatterns = [
-    url(r'graphql', GraphQLView.as_view(graphiql=True)),
+    path('admin/', admin.site.urls),
+    path("graphql", GraphQLView.as_view(graphiql=True)),
 ]
