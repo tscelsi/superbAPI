70ebf08792f2a53c7c65dc22a91fae942790af8c
tscelsi <tscelsi@student.unimelb.edu.au>
2022-03-13 13:55:51 +1100

    GraphQL: added the appropriate query endpoints and unittests for those endpoints

--- app/schema.py
+++ app/schema.py
@@ -1,6 +1,23 @@
 import graphene
+from graphene_django import DjangoObjectType
+from app.models import Person, Address
+
+class PersonType(DjangoObjectType):
+    class Meta:
+        model=Person
+        fields="__all__"
+
+class AddressType(DjangoObjectType):
+    class Meta:
+        model=Address
+        fields="__all__"
 
 class Query(graphene.ObjectType):
-    hello = graphene.String(default_value="Hi!")
+    person = graphene.Field(PersonType)
+
+    def resolve_person(root, info):
+        # here we just default return the only entry in our database right now. Me!
+        person = Person.objects.get()
+        return PersonType(email=person.email, name=person.name, address=person.address)
 
 schema = graphene.Schema(query=Query)
--- app/tests.py
+++ app/tests.py
@@ -1,3 +1,57 @@
-from django.test import TestCase
+"""
 
-# Create your tests here.
+Author: Thomas Scelsi
+Email: tlscelsi@gmail.com
+
+This file covers the testing of graphql query endpoints. 
+It is inspired somewhat from the graphene test suite found at:
+https://github.com/graphql-python/graphene-django/blob/master/graphene_django/tests/test_query.py
+
+"""
+
+from django.test import TestCase
+from app.schema import schema
+from app.models import Address, Person
+
+class SimpleTestCase(TestCase):
+    def setUp(self) -> None:
+        # create entry in test database
+        a1 = Address.objects.create(number=47,street="Magnolia Court",city="Melbourne",state="VIC")
+        p1 = Person.objects.create(name="Thomas Scelsi",email="tlscelsi@gmail.com",address=a1)
+        return super().setUp()
+
+    def test_should_return_correct_user_info(self):
+        query = '''
+            query {
+                person {
+                    email
+                    name
+                    address {
+                    number
+                    street
+                    city
+                    state
+                    }
+                }
+            }
+        '''
+        result = schema.execute(query)
+        # we check to see if there are any status code errors
+        assert not result.errors
+        # let's make sure that the information contained in the query response is as expected.
+        expected = {
+            "person": {
+                "name": "Thomas Scelsi",
+                "email": "tlscelsi@gmail.com",
+                "address": {
+                    "number": 47,
+                    "street": "Magnolia Court",
+                    "city": "Melbourne",
+                    "state": "VIC"
+                }
+            }
+        }
+        assert result.data == expected, str(result.data)
+        
+        
+        
